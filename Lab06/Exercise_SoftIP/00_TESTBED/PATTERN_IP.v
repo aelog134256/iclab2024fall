500d065ea82db768aaf286e4de47ef5f91e90ce7
Allen Kuan <c20kyo1827@gmail.com>
2024-10-18 13:55:42 +0800

    fix: give the wrong data to the IP in ip pattern

--- Lab06/Exercise_SoftIP/00_TESTBED/PATTERN_IP.v
+++ Lab06/Exercise_SoftIP/00_TESTBED/PATTERN_IP.v
@@ -44,7 +44,7 @@ integer   SEED = 54871;
 parameter ERR_NUM = 2;
 parameter ERR_DEN = 2;
 //
-parameter DEBUG = 0;
+parameter DEBUG = 1;
 //^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 parameter CYCLE = `CYCLE_TIME;
 
@@ -485,7 +485,7 @@ task encode_data_task; begin
 end endtask
 
 task input_task; begin
-    IN_code = _data;
+    IN_code = _encodeDataWithErr;
     @(negedge clk);
 end endtask
 
